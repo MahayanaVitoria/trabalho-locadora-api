505b68f90108a70bb4e96ac131142799f90137fe
Bruno Oliveira Calisario <bruno145224@gmail.com>
2022-11-05 23:24:32 -0300

    more changes

---
+++ frontend/frontend/static/js/index.js
@@ -0,0 +1,68 @@
+import Cadastro from "./views/Cadastro";
+import Catalogo from "./views/Catalogo";
+import Menu from "./views/Menu";
+
+
+
+const getParams = match => {
+    const values = match.result.slice(1);    
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+    console.log(Array.from(match.route.path.matchAll(/:(\w+)/g)))
+
+    return {}
+}
+
+
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+};
+
+const router = async () => {
+    // console.log(pathToRegex("/posts/:id"))
+    // /filmes/:id
+    const routes = [
+        { path: "/",            view: Menu },
+        { path: "/cadastro",    view: Cadastro },
+        { path: "/filmes/:id/:dcode",  view: Catalogo},
+        { path: "/catalogo",    view: Catalogo }
+    ];
+
+    // Test each route for match
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            result: location.pathname.match(pathToRegex(route.path))
+        }
+    })
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
+    
+    if (!match) {
+        match = {
+            route: routes[0],
+            isMatch: True
+        };
+    }
+
+    const view = new match.route.view(getParams(match));
+
+    document.querySelector("#app").innerHTML = await view.getHtml();
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    })
+
+    router();
+})
